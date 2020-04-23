2124c30522acc72bb932a24942097a2c565acb97
Melvin Evangelista <melvs.evangelista@gmail.com>
2020-04-23 15:42:07 +0800

    added redux and impleted reducers and actions

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+const composeEnhancers = composeWithDevTools({});
+
+const middlewares = [logger];
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
+
+export default store;
