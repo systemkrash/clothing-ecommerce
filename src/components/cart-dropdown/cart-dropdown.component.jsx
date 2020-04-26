95f93341780bef0b90dbf6e9a532016d2e396f6e
Melvin Evangelista <melvs.evangelista@gmail.com>
2020-04-26 09:28:53 +0800

    added addItem and removeItem on checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import {withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
-import {toggleCartHidden} from '../../redux/cart/cart.actions';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-dropdown.styles.scss';
 
@@ -22,12 +22,14 @@ const CartDropdown = ({ cartItems, history, dispatch }) => {
           <span className="empty-message">Your cart is empty</span>
         )}
       </div>
-      <CustomButton onClick={
-        () => {
+      <CustomButton
+        onClick={() => {
           history.push('/checkout');
           dispatch(toggleCartHidden());
-        }
-      }>GO TO CHECKOUT</CustomButton>
+        }}
+      >
+        GO TO CHECKOUT
+      </CustomButton>
     </div>
   );
 };
