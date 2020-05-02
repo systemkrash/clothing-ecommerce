5eaefdaa19ab980d74a395129379ea6a8ecc74b4
Melvin Evangelista <melvs.evangelista@gmail.com>
2020-05-02 19:09:30 +0800

    implement code to pull data from firestore

---
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
+
+const WithSpinner = (WrapComponent) => {
+  const Spinner = ({ isLoading, ...otherProps }) => {
+    return isLoading ? (
+      <SpinnerOverlay>
+        <SpinnerContainer />
+      </SpinnerOverlay>
+    ) : (
+      <WrapComponent {...otherProps} />
+    );
+  };
+
+  return Spinner;
+};
+
+export default WithSpinner;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,78 @@
-import React from 'react';
-import {Route} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({match}) => {
-  return (
-    <div className="shop-page">
-      <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    </div>
-  );
-};
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
 
-export default ShopPage;
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+import { updateCollections } from '../../redux/shop/shop.actions';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+class ShopPage extends Component {
+  state = {
+    loading: true,
+  };
+
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection('collections');
+
+    // fetch('https://firestore.googleapis.com/v1/projects/clothingdb-b623a/databases/(default)/documents/collections')
+    //   .then(response => response.json())
+    //   .then(collections => console.log(collections));
+
+    collectionRef.get().then((snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
+
+    // Obsevable
+    // collectionRef.onSnapshot(async (snapshot) => {
+    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+
+    //   updateCollections(collectionsMap);
+    //   this.setState({ loading: false });
+    // });
+  }
+
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+
+    return (
+      <div className="shop-page">
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          render={(props) => (<CollectionPageWithSpinner isLoading={loading} {...props} />)}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
